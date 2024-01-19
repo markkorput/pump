bcd6738343a6eacfe2e34fcf16d2c6eb985ee6cb
Mark van de Korput <mark@cleverfranke.com>
2024-01-19 14:38:13 +0100

    publish config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export'
+    output: 'export',
+    basePath: isProd ? process.env.PROD_BASE_PATH : undefined
 };
 
 export default nextConfig;
